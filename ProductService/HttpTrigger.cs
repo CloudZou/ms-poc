5726331abc1b11c5b815c0bbb008b4b2cf5b8540
CloudZou <Cloud.Zou@quest.com>
2019-08-14 16:47:03 +0800

    cc

---
+++ ProductService/HttpTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+
+namespace ProductService
+{
+    public class HttpTrigger
+    {
+        private readonly HttpClient client;
+
+        public HttpTrigger(IHttpClientFactory httpClientFactory)
+        {
+            client = httpClientFactory.CreateClient();
+        }
+
+        [FunctionName("GetPosts")]
+        public async Task<IActionResult> Get([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "posts")]
+            HttpRequest req, ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            var res = await client.GetAsync("https://microsoft.com");
+            return new OkResult();
+        }
+    }
+}
---
+++ ProductService/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using ProductService;
+
+[assembly:FunctionsStartup(typeof(ProductServiceApp))]
+namespace ProductService
+{
+    public class ProductServiceApp: FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddHttpClient();
+//            builder.Services.AddMediatR();
+
+        }
+    }
+}
